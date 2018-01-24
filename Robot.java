01e6a41fed490bbdfecacfeb86c99fb89b8a1b73
SMA Robotics Account <SMA Robotics Account>
2018-01-24 10:09:04 -0800

    pushing off broken laptop

--- Robot.java
+++ Robot.java
@@ -1,91 +1,88 @@
 package org.usfirst.frc.team5975.robot;
 
-//import edu.wpi.first.wpilibj.DigitalInput;
-//import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.SensorBase;
-import edu.wpi.first.wpilibj.InterruptableSensorBase;
-import edu.wpi.first.wpilibj.DigitalSource;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class Robot extends IterativeRobot {
 	RobotDrive myRobot;
 	VictorSP leftMotor;
 	VictorSP rightMotor;
-	VictorSP liftMotor;
-	Ultrasonic distanceSensor;
-	DigitalInput weCanGoTheDistance;
-	DigitalOutput weWentTheDistance;
+	
 	
 	// RoboRio mapping
 	int leftMotorChannel=1;
 	int rightMotorChannel=2;
-	int liftMotorChannel=4;
-	
-	//IO pin mapping
-	int echoSensorPin=6;
-	int pingSensorPin=7;
+	DriverStation ds = DriverStation.getInstance();
+
 	
 	// Driver Station / controller mapping
 	int joyPort1=0; //driver xbox controller
 	int joyPort2=1; //lift xbox controller
 	
-	int lTriggerID = 2;
-	int rTriggerID = 3;
+	//Driver Controls
 	int leftStickID = 1;
 	int rightStickID = 5;
-	int turboButtonID = 6;
+	int antiTurboButtonID = 6;
+	int ifThisIsTooFastForYouPushThisButtonID = 5;
 	Joystick youDriveMeCrazzy;
 	Joystick youManipulateMyHeart;
+	
+	//Manipulator Controls
+	int leftTriggerID = 2; //intake cube
+	int rightTriggerID = 3; //eject cube
+	int leftBumperID = 5; //descend elevator
+	int rightBumberID = 6; //ascend elevator
+	
+	//Driver stations
+	int positionNumber = 2;
+	int station1 = 1; //leftmost station
+	int station2 = 2; //center station
+	int station3 = 3; //rightmost station
+	
 	//verify that stick1 and stick2 correspond to the left and right joysticks on the controller
+	//6 is right, 5 is left
 	 
-	// maximum values adjustments need to be made
-	double inchesFromWall = 8.0;
 	
 	//global variables
 	int moveCounter;
 	int moveLimit;
 	double driveLeft;
 	double driveRight;
-	boolean ultraTrigger;
 	int rotationCounter;
+	int turnLimit;
+	int moveToPegLimit;
+	boolean scaleGamePiece;
+	boolean closeSwitchPiece;
+	boolean farSwitchPiece;
+	//for field pieces, right is true, left is false
+	
 	
 	//digital inputs
-	double speedLimitFactor = 0.8;
+	double speedLimit = 0.8;
+	double extremeSpeedLimit = 0.5;
+	double normalMode = 0.75;
 
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
+    	
     	leftMotor = new VictorSP(leftMotorChannel);
     	rightMotor = new VictorSP(rightMotorChannel);
-    	liftMotor = new VictorSP(liftMotorChannel);
+   
     	myRobot = new RobotDrive(leftMotor,rightMotor);
     	leftMotor.setInverted(false);
-    	/*weCanGoTheDistance = new DigitalInput(9);
-    	weWentTheDistance = new DigitalOutput(9);
-    	distanceSensor = new Ultrasonic(weWentTheDistance, weCanGoTheDistance);
-    	distanceSensor.setAutomaticMode(true);
-    	if (distanceSensor.isEnabled()) {
-    		System.out.println("Distance sensor is enabled");
-    	} else {
-    		System.out.println("Distance sensor is not enabled");
-    	}
-    	*/
+   
+    	
+    	
     	youDriveMeCrazzy  = new Joystick(joyPort1);
     	youManipulateMyHeart = new Joystick(joyPort2);
-    	
-        //test mode
-    	//LiveWindow.addSensor("driveSystem", "distanceSensor", distanceSensor);
-    	//SmartDashboard.putNumber("DistanceSensor", distanceSensor.getRangeInches());
     }
     
     /**
@@ -93,32 +90,60 @@ public class Robot extends IterativeRobot {
      */
     public void autonomousInit() {
     	moveCounter = 0;
-    	moveLimit = 213;
-    	driveLeft = 0.5;
-    	driveRight = 0.5;
+    	moveLimit = 270;
+   
+    	driveLeft = 0.9;
+
+    	driveRight = 0.9;
+    	
+    	String gameData;
+		gameData = DriverStation.getInstance().getGameSpecificMessage();
+		if(gameData.charAt(0) == 'L')//switch closest to our robot, determining whether our team color is on the left or right
+		{
+			closeSwitchPiece = false;//Left
+		} else {
+			closeSwitchPiece = true;//Right
+		}
+		if(gameData.charAt(1) == 'L')//telling which color our side is on the big scale
+		{
+			scaleGamePiece = false;//Left
+		} else {
+			scaleGamePiece = true;//Right
+		}
+		if(gameData.charAt(2) == 'L')//switch farthest from our robot, determining whether our team color is on the left or right
+		{
+			farSwitchPiece = false;//Left
+		} else {
+			farSwitchPiece = true;//Right
+		}
     }
+   
     
     /**
      * This function is called periodically during autonomous
      */
-	
+
     public void autonomousPeriodic() {
     	System.out.println(moveCounter);
-    	if (moveCounter < moveLimit)
+    		
+    	if ((positionNumber == station1) || (positionNumber == station3)) 
     	{
-    		myRobot.tankDrive(driveLeft, driveRight);
-    		moveCounter++;
+    		if (moveCounter < moveLimit)
+    		{
+    			moveCounter++;
+    			//bigger the number for curve (second parameter) the more left it will turn
+    			myRobot.drive(0.3, -0.00002);
+    		}
+  
     	}
-    	else {
-    		myRobot.drive(0.0, 0.0); 	// stop robot
-    	}
+    	
     }
     
     /**
      * This function is called once each time the robot enters tele-operated mode
      */
+    	
     public void teleopInit(){
-    	ultraTrigger = false;
     	rotationCounter = 0;
     }
 
@@ -129,8 +154,8 @@ public class Robot extends IterativeRobot {
     public void teleopPeriodic() {
     	// went left instead of right
     	
-    	double leftAxis = -youDriveMeCrazzy.getRawAxis(leftStickID);
     	double rightAxis = -youDriveMeCrazzy.getRawAxis(rightStickID);
+    	double leftAxis = -youDriveMeCrazzy.getRawAxis(leftStickID);
     
     	leftAxis = limitAxis(leftAxis);
     	rightAxis = limitAxis(rightAxis);
@@ -139,30 +164,15 @@ public class Robot extends IterativeRobot {
     	rightAxis = limitSpeed(rightAxis);
     
     	myRobot.tankDrive(leftAxis, rightAxis);
-        
-    	double myLAxis =  youDriveMeCrazzy.getRawAxis(lTriggerID); 
-    		//System.out.println(myLAxis);
-    	if (myLAxis > 0 && ultraTrigger == false){
-    		System.out.println(distanceSensor.getRangeInches());
-    		ultraTrigger = true;
-    	}
-    	if (myLAxis == 0){
-    		ultraTrigger = false;
-    	}
-     	
-    	//positive goes up, negative goes down
-    	climbRope ();
-    	
     }
-    
-   //limits joystick axis to range -1.0 to 1.0
+    	
     private double limitAxis (double axis) {
     	
     	if (axis > 1.0)
     	    axis = 1.0;
     	else if (axis < -1.0)
     		axis = -1.0;
-    	// if not turbo mode slow down robot to 50% 
+     
     	
     	return axis;
     		
@@ -173,62 +183,21 @@ public class Robot extends IterativeRobot {
     	
     	axis = axis * axis * axis;
     	
-    	if(youDriveMeCrazzy.getRawButton(turboButtonID) == false){
-    		axis = axis * speedLimitFactor;
-    	} else {
-    		System.out.println("Turbo mode!!");
+    	if(youDriveMeCrazzy.getRawButton(antiTurboButtonID) == true){
+    		axis = axis * extremeSpeedLimit;
+    		System.out.println("Extremely Slow mode");
+    	} //this is the right button
+    	
+    	else if(youDriveMeCrazzy.getRawButton(ifThisIsTooFastForYouPushThisButtonID) == true){
+    		axis = axis * speedLimit;
+    		System.out.println("ifThisIsTooFastForYouPushThisButton mode");
+    	} //this is the left button
+    	 else {
+    		 axis = axis * normalMode;
     	}
+    	
     	
     	return axis;
     }
-
-    public double getLiftAxis()
-    {
-   	 // read trigger buttons and create the axis value
-    	 
-   	 double myLAxis =  youManipulateMyHeart.getRawAxis(lTriggerID); 
-   	 return limitAxis( myLAxis);
-    }
-    
-    public void climbRope()
-    {
-   
-    	liftMotor.set(27);
-    	longClimb ();
-    	shortClimb ();
-    	 
-    }
-    
-    //when right trigger pulled the motor has a set rotation allowed, 
-    //no matter how many times the trigger is pulled and let go of
-    public void longClimb() 
-    {
-    	double myRAxis =  youManipulateMyHeart.getRawAxis(lTriggerID);
-    	if (myRAxis > 0) {
-    		if (rotationCounter < 1) {
-    			liftMotor.set(.85);
-    			rotationCounter++;
-    		}
-    	}
-    }
-    
-    //when left trigger pulled the motor rotates set amount
-    public void shortClimb () 
-    {
-    	double myLAxis =  youManipulateMyHeart.getRawAxis(lTriggerID); 
-    	if (myLAxis > 0) {
-    		liftMotor.set(.27);
-    	}
-    }
-
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    	LiveWindow.run();
-    }
     
 }
-
-//rip -Sammy
